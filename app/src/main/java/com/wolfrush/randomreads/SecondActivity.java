162b6bd504b698ff71cd6bade7e124714b99d3cb
Jason Donnelly <skeleslumber@gmail.com>
2019-08-20 18:51:01 +0100

    fontSize - 2 options

--- app/src/main/java/com/wolfrush/randomreads/SecondActivity.java
+++ app/src/main/java/com/wolfrush/randomreads/SecondActivity.java
@@ -5,13 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDelegate;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.RadioButton;
 import android.widget.Switch;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
     SharedPref sharedpref;
+    RadioButton rad_sml;
+    RadioButton rad_big;
+    TextView story_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +29,38 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        rad_sml = (RadioButton) findViewById(R.id.rad_sml);
+        rad_big = (RadioButton) findViewById(R.id.rad_big);
+
 
         String savedExtra = getIntent().getStringExtra("text_thing");
-        TextView story_text = (TextView) findViewById(R.id.story_text);
+        final TextView story_text = (TextView) findViewById(R.id.story_text);
         story_text.setText(savedExtra);
 
         story_text.setMovementMethod(new ScrollingMovementMethod());
 
+        rad_sml.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                story_text.setTextSize(20);
+
+            }
+
+        });
+
+        rad_big.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                story_text.setTextSize(40);
+
+            }
+
+        });
+
     }
+
+
 
 }
