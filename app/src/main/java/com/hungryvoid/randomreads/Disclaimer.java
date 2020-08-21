2594c8fd14e9e03040e857bd8f01f7673727d77c
Jason Donnelly <skeleslumber@gmail.com>
2020-08-21 22:51:46 +0100

    Privacy policy added Zoom button updated

--- app/src/main/java/com/hungryvoid/randomreads/Disclaimer.java
+++ app/src/main/java/com/hungryvoid/randomreads/Disclaimer.java
@@ -3,13 +3,16 @@ package com.hungryvoid.randomreads;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class Disclaimer extends AppCompatActivity {
 
     SharedPref sharedpref;
+    Button privacy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { //The Night Mode code again
@@ -35,11 +38,20 @@ public class Disclaimer extends AppCompatActivity {
 
         });
 
+        privacy=(Button) findViewById(R.id.privacy);
+
+        privacy.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent privacyIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://hungryvoid.github.io/PrivacyPolicy/")); //This line connects the button to the website
+                startActivity(privacyIntent);
+            }
+        });
+
     }
 
     @Override //This overrides the phones back button to do the same thing as my back buttons
     public void onBackPressed() {
-        Intent myIntent = new Intent(getBaseContext(), MainActivity.class);
+        Intent myIntent = new Intent(getBaseContext(), Settings.class);
         startActivity(myIntent);
         finish();
     }
